d04e56940c9abcf79c53633c947d4e354089716f
Pallavi <pallavidaneshrpt@gmail.com>
2024-07-19 23:03:49 +0530

    String

---
+++ String/Minerals.java
@@ -0,0 +1,14 @@
+class Minerals{
+	public static void iron(){
+		double weight=121.34;
+		System.out.println("atomic weight of iron is: "+weight);
+		String scientificName="Ferrous";
+		weight=232.67;
+		System.out.println("atomic weight of iron is: "+weight);
+		System.out.println("scientific name of iron is:"+scientificName);
+		}
+		public static void main(String[]args){
+			iron();
+		}
+}
+	
